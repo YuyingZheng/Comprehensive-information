0815aa4bb6e1de777e03960d522985dfa5aa00bd
LeapInMarch <350418663@qq.com>
2018-02-05 15:01:36 +0800

    svg-sprites

--- comprehensive/SVG整合/bridge/gulpfile.js
+++ comprehensive/SVG整合/bridge/gulpfile.js
@@ -1,28 +1,70 @@
 var gulp = require('gulp'),
     svgstore = require('gulp-svgstore'),
     svgmin = require('gulp-svgmin'),
+    svgSprite = require('gulp-svg-sprite'),
+    svgSymbols = require('gulp-svg-symbols'),
     path = require('path'),
-    rename = require('gulp-rename')
-    clean = require('gulp-clean');
+    rename = require('gulp-rename'),
+    clean = require('gulp-clean'),
+    sequence = require('gulp-sequence');
 
 
-var cleanFolder =['../dist/'];
+var cleanFolder =['../dist/**'];
 
 gulp.task('clean',function(){
     return gulp.src(cleanFolder,{read:false})
     .pipe(clean({force:true}));
 });
 /* -- */
-var iconSrc = '../src2/*.svg',
-    iconDist = '../dist/';
+var iconSrc = '../icon/*.svg',
+    iconSrc2 = '../icon2/*.svg',
+    iconDest = '../dist/';
 
-gulp.task('integrate-svg',function(){
+gulp.task('svg-sprites',function(){
     return gulp.src(iconSrc)
         .pipe(svgmin())
         .pipe(rename({
             prefix:'icon-'
         }))
         .pipe(svgstore())
-        .pipe(gulp.dest(iconDist));
+        .pipe(rename("icons.svg"))
+        .pipe(gulp.dest(iconDest));
 });
-gulp.task('default', ['clean','integrate-svg']);
+/* gulp.task('integrate-svg-2',function(){
+    return gulp.src(iconSrc2)
+        .pipe(svgmin())
+        .pipe(rename({
+            prefix:'icon-'
+        }))
+        .pipe(svgstore())
+        .pipe(rename("icons2.svg"))
+        .pipe(gulp.dest(iconDest));
+}); */
+/* gulp.task('integrate-svg-2', function(){
+    return gulp.src('iconSrc')
+        .pipe(svgSprite({
+            shape: {
+                dimension: {
+                    maxWidth: 32,
+                    maxHeight: 32
+                }
+            },
+            mode: {
+                symbol:true
+            }
+        }))
+        .pipe(rename("icons2.svg"))
+        .pipe(gulp.dest('iconDist'));
+}); */
+gulp.task('svg-sprites-2',function(){
+    return gulp.src(iconSrc)
+        .pipe(rename({
+            prefix:'icon-'
+        }))
+        .pipe(svgSymbols())
+        
+        .pipe(gulp.dest(iconDest));
+});
+gulp.task('default',function(cb){
+    sequence('clean',['svg-sprites','svg-sprites-2'])(cb);
+}); 
